5dba80e0c4313ddbb9a1872166033e3b4ae5ad4a
SeopBong <gkstjq309@gmail.com>
2023-07-12 18:00:54 +0900

    iconAdd

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ import SecondPart from './ContainerPart/components/SecondPart';
 import ThirdPart from './ContainerPart/components/ThirdPart';
 import FivePart from './ContainerPart/components/FivePart';
 import FiveAdd from './ContainerPart/components/FiveAdd';
+import TVImage from './image/Index_TV.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App(props) {
-  const aboutWod_0 = "비전검사기로 어떤 것을 할 수 있나요?";
-  const aboutWod_1 = "비전검사는 제품을 촬영하여 영상분석기술을 이용해 검사하는 장비로\n 사람 눈으로 구분이 가능한 모든 불량부터 사람눈으로 어려운 정밀 불량까지 모두 검출할 수 있습니다";
-  const reect_1 = 1;
-
-
+  const wodPart_1= "비전검사기로 어떤 것을 할 수 있나요?";
+  const wodPart_11 = "비전검사는 제품을 촬영하여 영상분석기술을 이용해 검사하는 장비로\n 사람 눈으로 구분이 가능한 모든 불량부터 사람눈으로 어려운 정밀 불량까지 모두 검출할 수 있습니다";
+  
   let [navbarColor, setNavbarColor] = useState('dark'); // 네비게이션 바 색상 상태
+
   let color =  useEffect(() => {
     let handleScroll = () => {
       // 스크롤 위치에 따라 색상 변경
@@ -34,6 +35,9 @@ function App(props) {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+
+
 
 
 /***********************************************************************************************************************************************/
@@ -75,11 +79,16 @@ function App(props) {
       </div>
 
 
-      <div className='onerlay-2' style={{ whiteSpace: 'pre-line' }}>
-      <h2>{aboutWod_0}</h2>
-      <p>{aboutWod_1}</p> 
+      <div className='onerlay-2' style={{ whiteSpace: 'pre-line' }}>  
+      <h2>{wodPart_1}</h2>
+      <p>{wodPart_11}</p> 
+      <div className='img-section-change'>
       
+      <img src={TVImage}></img>
+      </div>
+
       <div className='onerlay-2-skills'>
+        
         <SecondPart text="요소1" />
         <SecondPart text="요소2" />
         <SecondPart text="요소3" />
--- src/ContainerPart/components/SecondPart.js
+++ src/ContainerPart/components/SecondPart.js
@@ -1,11 +1,17 @@
 import './SecondPart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 function SecondPart(props) {
     return(
         <div className='box-with-text'>
-        <div className='empty-box'></div>
-        <p className='text'>{props.text}</p></div>
-    )
+        <FontAwesomeIcon icon={faUser} />
+        {/* <div className='empty-box'></div> */}
+        <p className='text'>{props.text}</p>
+
+        </div>
+        
+    );
 };
 
 export default SecondPart;
